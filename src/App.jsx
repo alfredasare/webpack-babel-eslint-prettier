056b80c40b151c877fdd57ede7fcc44dd8e86169
Alfred Asare <alfredasare101@gmail.com>
2020-07-03 20:02:14 +0000

    testing eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './components/header/header.component';
 
 const App = () => {
+  useEffect(() => {
+    console.log('Something');
+  }, []);
+
   return (
     <div>
       <Header />
